5ea2a85f9868d83c175c32e1ccac1e890595469d
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2022-07-14 20:44:19 +0430

    Init: console app

---
+++ Windows Application/DC Motor PID Controller/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace DC_Motor_PID_Controller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ali Ghadyani - Mohammad Esfandiarpour";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baud Rate:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(21, 49);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(277, 142);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Communication";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 108);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 108);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(107, 69);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(159, 24);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(53, 69);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 17);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Port:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AccessibleName = "baudRate";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "57600",
+            "115200",
+            "250000"});
+            this.comboBox1.Location = new System.Drawing.Point(107, 31);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(159, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox3);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(21, 197);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(277, 167);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "PID";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "PID",
+            "PI",
+            "PD",
+            "P",
+            "I",
+            "D"});
+            this.comboBox3.Location = new System.Drawing.Point(85, 138);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(169, 24);
+            this.comboBox3.TabIndex = 8;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(21, 138);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 17);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Mode";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(85, 59);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(173, 22);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.Text = "0.0339782106923304";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(85, 97);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(173, 22);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "3.14697363162208e-05";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 21);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0.00206812507453938";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 63);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Ki:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 101);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Kd:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 25);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kp:";
+            // 
+            // chart
+            // 
+            this.chart.Location = new System.Drawing.Point(309, 59);
+            this.chart.Margin = new System.Windows.Forms.Padding(4);
+            this.chart.Name = "chart";
+            this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            this.chart.Size = new System.Drawing.Size(624, 404);
+            this.chart.TabIndex = 5;
+            this.chart.Text = "chart1";
+
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.numericUpDown1);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(21, 364);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(277, 98);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Motor";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(85, 30);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(171, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 69);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 17);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Time:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(85, 69);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 22);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "hi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 30);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Speed:";
+            //
+            // serialPort1
+            //
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.readSerial);
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 475);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "DC Motor PID Controller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Windows Application/DC Motor PID Controller/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Threading;
+
+namespace DC_Motor_PID_Controller
+{
+    public partial class Form1 : Form
+    {
+        double Kp = 0.003;
+        double Ki = 0.003;
+        double Kd = 0.003;
+        double Speed = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedItem = comboBox1.Items[5];
+            //comboBox1.Enabled = false;
+            String[] portList = SerialPort.GetPortNames();
+            comboBox2.Items.AddRange(portList);
+            if(portList.Length > 0)
+            {
+                comboBox2.SelectedItem = comboBox2.Items[0];
+            }
+            comboBox2.Enabled = false;
+            button2.Enabled = false;
+            comboBox3.SelectedItem = comboBox3.Items[0];
+
+            //serialPort1.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.Text=="" || comboBox2.Text == "")
+            {
+                return;
+            }
+            serialPort1.PortName = comboBox2.Text;
+            serialPort1.BaudRate = Int32.Parse(comboBox1.Text);
+            serialPort1.Open();
+            if(!serialPort1.IsOpen)
+            {
+                MessageBox.Show("Faild to connect to the device!");
+                return;
+            }
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            if (serialPort1.IsOpen)
+            {
+                MessageBox.Show("Faild to disconnect the device!");
+                return;
+            }
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(!serialPort1.IsOpen)
+            {
+                return;
+            }
+            if(comboBox3.Text == "PID")
+            {
+                textBox2.Enabled = true;
+                textBox2.Text = Kp.ToString();
+                textBox5.Enabled = true;
+                textBox5.Text = Ki.ToString();
+                textBox4.Enabled = true;
+                textBox4.Text = Kd.ToString();
+                sendCommand();
+            }
+            else if(comboBox3.Text == "PI")
+            {
+                textBox2.Enabled = true;
+                textBox2.Text = Kp.ToString();
+                textBox5.Enabled = true;
+                textBox5.Text = Ki.ToString();
+                textBox4.Enabled = false;
+                Kd = (textBox4.Text != (0.0).ToString()) ? Convert.ToDouble(textBox4.Text) : Kd;
+                textBox4.Text = (0.0).ToString();
+                sendCommand();
+            }
+            else if (comboBox3.Text == "PD")
+            {
+                textBox2.Enabled = true;
+                textBox2.Text = Kp.ToString();
+                textBox5.Enabled = false;
+                Ki = (textBox5.Text != (0.0).ToString()) ? Convert.ToDouble(textBox5.Text) : Ki;
+                textBox5.Text = (0.0).ToString();
+                textBox4.Enabled = true;
+                textBox4.Text = Kd.ToString();
+                sendCommand();
+            }
+            else if (comboBox3.Text == "P")
+            {
+                textBox2.Enabled = true;
+                Kp = Convert.ToDouble(textBox2.Text);
+                textBox2.Text = Kp.ToString();
+                textBox5.Enabled = false;
+                Ki = (textBox5.Text != (0.0).ToString()) ? Convert.ToDouble(textBox5.Text) : Ki;
+                textBox5.Text = (0.0).ToString();
+                textBox4.Enabled = false;
+                Kd = (textBox4.Text != (0.0).ToString()) ? Convert.ToDouble(textBox4.Text) : Kd;
+                textBox4.Text = (0.0).ToString();
+                sendCommand();
+            }
+            else if (comboBox3.Text == "I")
+            {
+                textBox2.Enabled = false;
+                Kp = (textBox2.Text != (0.0).ToString()) ? Convert.ToDouble(textBox2.Text):Kp;
+                textBox2.Text = (0.0).ToString();
+                textBox5.Enabled = true;
+                textBox5.Text = Ki.ToString();
+                textBox4.Enabled = false;
+                Kd = (textBox4.Text != (0.0).ToString()) ? Convert.ToDouble(textBox4.Text) : Kd;
+                textBox4.Text = (0.0).ToString();
+                sendCommand();
+            }
+            else if (comboBox3.Text == "D")
+            {
+                textBox2.Enabled = false;
+                Kp = (textBox2.Text != (0.0).ToString()) ? Convert.ToDouble(textBox2.Text):Kp;
+                textBox2.Text = Kp.ToString();
+                textBox5.Enabled = false;
+                Ki = (textBox5.Text != (0.0).ToString()) ? Convert.ToDouble(textBox5.Text):Kd;
+                textBox5.Text = (0.0).ToString();
+                textBox4.Enabled = true;
+                textBox4.Text = Kd.ToString();
+                sendCommand();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            serialPort1.Write(numericUpDown1.Value.ToString());
+        }
+
+        private void sendCommand()
+        {
+            if(!serialPort1.IsOpen)
+            {
+                return;
+            }
+            String data = (1).ToString() + '|' +
+                            Convert.ToDouble(textBox2.Text).ToString() + '|' +
+                            Convert.ToDouble(textBox5.Text).ToString() + '|' +
+                            Convert.ToDouble(textBox4.Text).ToString() + '|' +
+                            Convert.ToDouble(numericUpDown1.Text).ToString();
+            serialPort1.WriteLine(data);
+            while (serialPort1.BytesToRead == 0) ;
+            textBox3.Text = serialPort1.ReadLine();
+        }
+
+        private void readSerial(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort1.ReadLine();
+            textBox3.Text = data;
+        }
+    }
+}
